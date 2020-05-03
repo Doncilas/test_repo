c394f6fa1ecaeb11fb5cb9a1e1f41b0c6059cdc7
Sergiu Doncila <sergiu.doncila@yahoo.com>
2020-05-03 13:49:10 +0300

    ser

---
+++ practice_BASE_2515.py
@@ -0,0 +1,34 @@
+class Car:
+    language = 'Python'
+    # def __init__(self):
+    #     self.brand = 'Ferrari'
+    #     self.collection = []
+
+    def hello():
+        print('hello {}')
+
+    def add_brands(self):
+        self.collection = []
+        self.collection.append('3')
+
+        return self.collection
+        
+        BAAAAAAAA
+
+print(type(Car.hello))
+
+print("Un change")
+
+print("Inca unul")
+
+print("Bai Sergiu")
+
+"fast forward"
+"""meergeeee""
+# bas.language = 'C++'
+# Car.language = 'Sharp'
+# bas.dupa = 'Adaug'
+
+# print(type(bas.__dict__))
+# print(type(Car.__dict__))
+
---
+++ practice_LOCAL_2576.py
@@ -0,0 +1,36 @@
+class Car:
+    language = 'Python'
+    # def __init__(self):
+    #     self.brand = 'Ferrari'
+    #     self.collection = []
+
+    def hello():
+        print('hello {}')
+        retur miau
+
+    def add_brands(self):
+    self_adaug_list = []
+        self.collection = []
+        self.collection.append('3')
+
+        return self.collection
+        
+        BAAAAAAAA
+
+print(type(Car.hello))
+
+print("Un change")
+
+print("Inca unul")
+
+print("Bai Sergiu")
+
+"fast forward"
+"""meergeeee""
+# bas.language = 'C++'
+# Car.language = 'Sharp'
+# bas.dupa = 'Adaug'
+
+# print(type(bas.__dict__))
+# print(type(Car.__dict__))
+
